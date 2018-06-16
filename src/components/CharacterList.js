facc79aa073c58bb6c944006bb5991eb04830352
Alberto Aragon <alberto@alturasoluciones.com>
2018-06-16 17:51:26 -0500

    Second Action: Removing Character. Character's Description. Some More Styling

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
-import { addCharacterById} from "../actions";
+import { addCharacterById } from "../actions";
 
 class CharacterList extends Component {
   render () {
     return (
-        <div className="col-md-4">
+        <div className="col-md-5 supers-list">
           <h4>Characters</h4>
           <ul className="list-group">
             {
               this.props.characters.map(character => {
                 return (
-                    <li key={ character.id } className="list-group-item">
-                      <div className="list-item">
+                    <li key={ character.id } className="list-group-item row">
+                      <div className="list-item col-md-10 text-center">
                         { character.name }
+                        <div className="row character-description-list">
+                          <div className="col-md-4 character-description-item">
+                            Strenght: { character.strength }
+                          </div>
+                          <div className="col-md-4 character-description-item">
+                            Intelligence: { character.intelligence }
+                          </div>
+                          <div className="col-md-4 character-description-item">
+                            Speed: { character.speed }
+                          </div>
+                        </div>
                       </div>
-                      <div onClick={ ()=> this.props.addCharacterById(character.id)} className="list-item right-button">
+                      <div onClick={ ()=> this.props.addCharacterById(character.id)} className="list-item right-button col-md-2 text-center btn btn-primary">
                         +
                       </div>
                     </li>
--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {addCharacterById} from "../actions";
+import { removeCharacterById } from "../actions";
 
 class HeroList extends Component {
   render() {
     return (
-        <div className="col-md-4">
+        <div className="col-md-5 supers-list">
           <h4>Heroes List</h4>
           <ul className="list-group">
             {
               this.props.heroes.map(hero => {
                 return (
-                    <li key={hero.id} className="list-group-item">
-                      <div className="list-item">
+                    <li key={hero.id} className="list-group-item row">
+                      <div className="list-item col-md-10 text-center">
                         {hero.name}
+                        <div className="row character-description-list">
+                          <div className="col-md-4 character-description-item">
+                            Strenght: { hero.strength }
+                          </div>
+                          <div className="col-md-4 character-description-item">
+                            Intelligence: { hero.intelligence }
+                          </div>
+                          <div className="col-md-4 character-description-item">
+                            Speed: { hero.speed }
+                          </div>
+                        </div>
                       </div>
-                      <div className="list-item right-button">
-                        -
+                      <div onClick={()=> this.props.removeCharacterById(hero.id)} className="list-item right-button col-md-2 text-center btn btn-danger">
+                        x
                       </div>
                     </li>
                 )
@@ -34,4 +45,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, {addCharacterById})(HeroList);
+export default connect(mapStateToProps, { removeCharacterById })(HeroList);
